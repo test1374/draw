ed23278a2a40fd1b87918c2a980a770f6e4e7463
Wet Takis <77040285+test1374@users.noreply.github.com>
2021-12-30 22:22:58 -0800

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express');
 
 var app = express();
-var server = app.listen(3000);
+var port = process.env.PORT
+var server = app.listen(port);
 
 app.use(express.static('public'));
 
